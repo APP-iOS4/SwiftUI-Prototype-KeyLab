aa1cb5c27cb68f907129ea4b67cb082f7510078d
JaeWoongJeong <acetate_aliens_0h@icloud.com>
2024-02-20 09:26:31 +0900

    first commit

---
+++ KeyLab/KeyLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeyLab
+//
+//  Created by woong on 2/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeyLab/KeyLab/KeyLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyLabApp.swift
+//  KeyLab
+//
+//  Created by woong on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct KeyLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
