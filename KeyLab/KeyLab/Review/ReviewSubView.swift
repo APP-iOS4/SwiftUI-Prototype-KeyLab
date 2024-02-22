cb7889f28adbdff57b56bc493f4e3b7e65bb2ec3
Jiho <yjh7827@naver.com>
2024-02-22 10:27:19 +0900

    Fix: [ReviewSubView] UI 수정, product detail flow 추가

--- KeyLab/KeyLab/Review/ReviewSubView.swift
+++ KeyLab/KeyLab/Review/ReviewSubView.swift
@@ -9,96 +9,100 @@ import SwiftUI
 
 struct ReviewSubView: View {
     var body: some View {
-        
-        ScrollView {
-            VStack {
-                Text("제목")
-                    .font(.title)
-                
-                ZStack(alignment: .topLeading) {
-                    Rectangle()
-                        .fill(Color.gray)
-                        .frame(width: 400, height: 300)
-                        .overlay(
-                            Image(systemName: "photo")
-                                .resizable()
-                                .foregroundColor(.black)
-                                .frame(width: 400, height: 150) // 이미지 크기 조정
-                        )
+        NavigationView {
+            ScrollView {
+                VStack {
                     
-                    HStack {
-                        Image(systemName: "person")
-                            .frame(width: 50, height: 50)
-                            .overlay {
-                                Circle()
-                                    .stroke()
-                            }
-                            .padding()
+                    Text("제목")
+                        .font(.title)
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        HStack {
+                            Image(systemName: "person")
+                                .frame(width: 40, height: 40)
+                                .overlay {
+                                    Circle()
+                                        .stroke()
+                                }
+                                .foregroundStyle(.mainorange)
+                            
+                            Text("닉네임") // 텍스트 추가
+                                .font(.headline) // 텍스트 크기 지정
+                            
+                            Spacer()
+                        }
+                        .padding()
                         
-                        Text("닉네임") // 텍스트 추가
-                            .font(.headline) // 텍스트 크기 지정
-                            .foregroundColor(.black) // 텍스트 색상 지정
-//                            .offset(x: 75, y: 25) // 위치 조정
+                        Image("keyboard1")
+                            .resizable()
+                            .frame(height: 250)
+                        
+                        VStack(alignment: .leading) {
+                            NavigationLink {
+                                ProductDetailView()
+                            } label: {
+                                Text("체리축, 흰색키캡, ABS하우징")
+                                    .font(.headline)
+                                    .padding(.bottom, 6)
+                            }
+                            
+                            Text("키보드 너무 멋져요~~")
+                        }
+                        .padding()
+                        .foregroundColor(.black)
                     }
-                    
-                    VStack { // 여러 텍스트를 수직으로 배치하는 VStack 추가
-                        Text("체리축, 흰색키캡, ABS하우징")
-                            .padding()
-                        Text("키보드 너무 멋져요~~") // 여러번 반복되는 텍스트
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(lineWidth: 0.5)
                     }
-                    .foregroundColor(.black) // 텍스트 색상 지정
-                    
-                    
-                    
-                    .offset(x: 5, y: 220) // 위치 조정
                     
                 }
+                .padding(.horizontal, 16)
+                .padding(.bottom, 36)
                 
-            }
-            .padding()
-            
-            VStack(alignment: .leading) {
-                Text("연관상품")
-                    .font(.title)
-                    .padding(.horizontal, 25)
-                ProductDoubleLineGridView()
+                VStack(alignment: .leading) {
+                    Text("연관상품")
+                        .font(.title)
+                        .padding(.horizontal, 16)
                     
-                    .padding(.bottom)
-                Text("댓글")
-                    .font(.title)
-                    .padding(.horizontal, 25)
-                
-                
-                ZStack(alignment: .topLeading) {
-                    Rectangle()
-                        .fill(Color.gray)
-                        .frame(width: 420, height: 150)
+                    ProductDoubleLineGridView()
+                        .padding(.bottom)
                     
-                    Circle()
-                        .fill(Color.white)
-                        .frame(width: 50, height: 50)
-                        .offset(x:22, y: 15)
-                    
-                    Text("하세기") // 텍스트 추가
-                        .font(.headline) // 텍스트 크기 지정
-                        .foregroundColor(.black) // 텍스트 색상 지정
-                        .offset(x: 85, y: 30) // 위치 조정
-                    
-                    VStack { // 여러 텍스트를 수직으로 배치하는 VStack 추가
-                        Text("체리축, 흰색키캡, ABS하우징")
-                            .padding()
-                        Text("키보드 너무 멋져요~~") // 여러번 반복되는 텍스트
-                    }
-                    .foregroundColor(.black) // 텍스트 색상 지정
-                    .offset(x: 5, y: 65) // 위치 조정
-                    
+                    //                Text("댓글")
+                    //                    .font(.title)
+                    //                    .padding(.horizontal, 25)
+                    //
+                    //                ZStack(alignment: .topLeading) {
+                    //                    Rectangle()
+                    //                        .fill(Color.gray)
+                    //                        .frame(width: 420, height: 150)
+                    //
+                    //                    Circle()
+                    //                        .fill(Color.white)
+                    //                        .frame(width: 50, height: 50)
+                    //                        .offset(x:22, y: 15)
+                    //
+                    //                    Text("하세기") // 텍스트 추가
+                    //                        .font(.headline) // 텍스트 크기 지정
+                    //                        .foregroundColor(.black) // 텍스트 색상 지정
+                    //                        .offset(x: 85, y: 30) // 위치 조정
+                    //
+                    //                    VStack { // 여러 텍스트를 수직으로 배치하는 VStack 추가
+                    //                        Text("체리축, 흰색키캡, ABS하우징")
+                    //                            .padding()
+                    //                        Text("키보드 너무 멋져요~~") // 여러번 반복되는 텍스트
+                    //                    }
+                    //                    .foregroundColor(.black) // 텍스트 색상 지정
+                    //                    .offset(x: 5, y: 65) // 위치 조정
+                    //
+                    //                }
                 }
             }
         }
         
         
-
-        Spacer()
+        
+        //        Spacer()
     }
 }
 
